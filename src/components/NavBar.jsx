e240c013487be9d80ced5e24934ac7d9f0355d7a
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-21 20:41:42 -0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 
 export default function NavBar(){
-    return <div className="flex justify-between items-center p-5 border-b-2 border-gray-200 shadow-md">
+    return <div className="flex justify-between items-center p-5 border-b-2 border-gray-200 shadow-md fixed top-0 left-0 right-0 z-50 bg-white backdrop-blur-sm">
         <div className= "text-2xl font-bold tracking-wider hover:text-slate-950 transition-all duration-300 text-slate-700 cursor-pointer hover:scale-105">
             FarmYadri
         </div>
@@ -23,7 +23,7 @@ export default function NavBar(){
             <div className="hover:text-slate-950 transition-all duration-300 text-slate-700 cursor-pointer">
                 Contact Us
             </div>
-            <button className="bg-slate-900 text-white px-4 py-2 rounded-md hover:bg-slate-800 transition-all duration-300">
+            <button className="bg-slate-900 text-white px-4 py-2 rounded-md hover:bg-slate-800 transition-all duration-300 hover:cursor-pointer">
                 Book Now
             </button>
         </div>
