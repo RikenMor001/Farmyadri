b271ca7caf3425a1b45b11a973d938f96e360a6d
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-08-03 00:37:36 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import NavBar from "./components/NavBar"
 import LandingPage from "./components/LandingPage"
 import About from "./components/About"
+import Gallery from "./components/Gallery"
 
 function App() { 
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<Gallery />} />
         </Routes>
       </div>
     </Router>
