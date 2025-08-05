1ffe2a609cfe06311d44a207a7d792a7cffe92ff
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-08-05 16:12:58 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import NavBar from "./components/NavBar"
 import LandingPage from "./components/LandingPage"
 import About from "./components/About"
 import Gallery from "./components/Gallery"
+import ContactUs from "./components/ContactUs"
 
 function App() { 
   return (
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<About />} />
           <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact-us" element = {<ContactUs/>}/>
         </Routes>
       </div>
     </Router>
